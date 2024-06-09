cdacc35383046ac54d17774e5934975eefcab633
Georgi Hadzhiev <georgi.hadzhiev@abv.bg>
2024-06-10 02:13:51 +0300

    updated the User model so it has propper attributes

--- sourceControlApp.Server/Models/User.cs
+++ sourceControlApp.Server/Models/User.cs
@@ -1,12 +1,27 @@
-﻿namespace sourceControlApp.Server.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace sourceControlApp.Server.Models
 {
     public class User
     {
 
+        [Key]
         public Guid Id { get; set; }
+
+        [Required]
+        [MaxLength(30)]
         public string FirstName { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(30)]
         public string LastName { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(50)]
         public string Email { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(30)]
         public string Password { get; set; } = string.Empty;
 
     }
