06ef368d4a47b225ec1806f8f942406e66c50136
Georgi Hadzhiev <georgi.hadzhiev@abv.bg>
2024-06-10 11:29:01 +0300

    added extra summeries for the rest of the constants

--- sourceControlApp.Server/Data/Constants.cs
+++ sourceControlApp.Server/Data/Constants.cs
@@ -4,10 +4,18 @@
     {
 
         /// <summary>
-        /// Constants for User models
+        /// Constant for User First name/Last Name 
         /// </summary>
         public const int UserNameLenght = 30;
+
+        /// <summary>
+        /// Constant for User Email
+        /// </summary>
         public const int EmailLenght = 50;
+
+        /// <summary>
+        /// Constant for User's Pasword Lenght
+        /// </summary>
         public const int PasswordLenght = 30;
 
     }
