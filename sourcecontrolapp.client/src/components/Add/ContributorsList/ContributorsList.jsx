317ae2dfb6a5e9845e02dc98893401c212869776
Georgi Hadzhiev <georgi.hadzhiev@abv.bg>
2024-06-13 19:20:13 +0300

    added some more styling to the "-" button

--- sourcecontrolapp.client/src/components/Add/ContributorsList/ContributorsList.jsx
+++ sourcecontrolapp.client/src/components/Add/ContributorsList/ContributorsList.jsx
@@ -1,13 +1,14 @@
 import ListGroup from 'react-bootstrap/ListGroup';
 import Button from 'react-bootstrap/Button';
+import classes from './ContributorList.module.css'
 
 // eslint-disable-next-line react/prop-types
 function ContributorsList({ contributor }) {
     return (
 
         <div>
-           <Button variant="success">-</Button>
-           <ListGroup.Item>{contributor}</ListGroup.Item>
+            <Button className={classes.button} variant="danger">-</Button>
+           <ListGroup.Item className={classes.list}>{contributor}</ListGroup.Item>
         </div>
 
     );
