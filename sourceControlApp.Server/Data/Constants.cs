37968b0dc5729db36c03f5487c8edd49cb9e8895
Georgi Hadzhiev <georgi.hadzhiev@abv.bg>
2024-11-18 03:52:51 +0200

    Created Repository data class to go into the Db

--- sourceControlApp.Server/Data/Constants.cs
+++ sourceControlApp.Server/Data/Constants.cs
@@ -32,12 +32,28 @@
         /// <summary>
         /// Messages displaied when user enters wrong credentials
         /// </summary>
+        #region "Wrong Credentials"
         public const string WrongEmailOrPass = "Email or Password is Incorrect!";
-
-        #region "Errors
+        #endregion
+        #region "Errors"
 
         public const string StringLenghtErrorMessage = "The field {0} must be between {2} and {1} characters long!";
 
+        #endregion
+
+        #region Repository Name lenghts
+        public const int RepoNameMax = 60;
+        public const int RepoNameMin = 1;
+        #endregion
+
+        #region Repository Description lenghts
+        public const int RepoDescriptionMax = 280;
+        public const int RepoDescriptionMin = 1;
+        #endregion
+
+        #region Repository Visiblitly lenght
+        public const int RepoVisibilityMax = 7;
+        public const int RepoVisibilityMin = 6;
         #endregion
 
     }
---
+++ sourceControlApp.Server/Data/Repository.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static sourceControlApp.Server.Data.Constants;
+
+namespace sourceControlApp.Server.Data
+{
+    public class Repository
+    {
+
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        //temporary number set here. Will remove once changed to upload file instead of string
+        [MaxLength(800)]
+        public string Code { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(RepoNameMax)]
+        public string RepoName { get; set; } = string.Empty;
+
+        [MaxLength(RepoDescriptionMax)]
+        public string Description { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(RepoVisibilityMax)]
+        public string Visibility { get; set; } = string.Empty;
+
+        public IList<string> Contributors = new List<string>();
+
+
+    }
+}
