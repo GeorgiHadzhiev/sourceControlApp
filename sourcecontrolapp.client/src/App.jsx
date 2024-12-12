cf9c0d9152c354e7f00d42bd3f7b12546a7dfeab
Georgi Hadzhiev <georgi.hadzhiev@abv.bg>
2024-12-12 04:20:47 +0200

    Added "Create" component to the App.jsx file

--- sourcecontrolapp.client/src/App.jsx
+++ sourcecontrolapp.client/src/App.jsx
@@ -7,7 +7,7 @@ import MyProfile from './components/MyProfile/MyProfile.jsx'
 import Dashboard from './components/Dashboard/Dashboard.jsx'
 import Login from './components/Login/Login'
 import Register from './components/Register/Register'
-import Add from './components/Add/Add.jsx'
+import Create from './components/Create/Create.jsx'
 import Logout from './components/Logout/Logout.jsx'
 
 function App() {
@@ -22,7 +22,7 @@ function App() {
                 <Route path="/Dashboard" element={<Dashboard />} />
                 <Route path="/login" element={<Login/>} />
                 <Route path="/register" element={<Register />} />
-                <Route path="/Add" element={<Add />} />
+                <Route path="/Add" element={<Create />} />
                 <Route path="/Logout" element={<Logout />} />
             </Routes>
 
