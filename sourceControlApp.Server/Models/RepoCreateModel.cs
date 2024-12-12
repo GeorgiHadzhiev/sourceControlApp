11e9c6e07884688df33588361eac1bb4293f9315
Georgi Hadzhiev <georgi.hadzhiev@abv.bg>
2024-12-12 08:58:54 +0200

    removed uneeded usings

--- sourceControlApp.Server/Models/RepoCreateModel.cs
+++ sourceControlApp.Server/Models/RepoCreateModel.cs
@@ -1,6 +1,4 @@
-﻿using sourceControlApp.Server.Data;
-using System.ComponentModel.DataAnnotations.Schema;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 using static sourceControlApp.Server.Data.Constants;
 
 namespace sourceControlApp.Server.Models
