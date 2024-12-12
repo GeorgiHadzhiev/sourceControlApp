f09a4da71ca7e1369caa07a8992edc41612f9282
Georgi Hadzhiev <georgi.hadzhiev@abv.bg>
2024-12-12 04:09:27 +0200

    Added an "add" function in the repoService.jsx file.

--- sourcecontrolapp.client/src/services/repoService.jsx
+++ sourcecontrolapp.client/src/services/repoService.jsx
@@ -1,1 +1,27 @@
-const baseUrl = 'https://localhost:7035/User'
+const baseUrl = 'https://localhost:7035/Repo'
+
+async function add(code, name, description, visibility, contributors, token) {
+
+    let res = await fetch(`${baseUrl}/add`, {
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json',
+            'X-Authorization': token,
+        },
+        body: JSON.stringify({ code, name, description, visibility, contributors })
+    })
+
+    let jsonResult = await res.json();
+
+    if (res.ok) {
+
+        return jsonResult;
+
+    }
+    else {
+
+        throw jsonResult.message;
+
+    }
+
+}
