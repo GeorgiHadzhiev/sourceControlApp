26089628a5d33bf4340cdb4ad8b4c3f9ea160689
Georgi Hadzhiev <georgi.hadzhiev@abv.bg>
2024-12-12 04:46:54 +0200

    Create "RepoController" class in backend

---
+++ sourceControlApp.Server/Controllers/RepoController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace sourceControlApp.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RepoController : ControllerBase
+    {
+
+        private readonly ILogger<RepoController> logger;
+
+
+        public RepoController(ILogger<RepoController> _logger)
+        {
+            logger = _logger;
+        }
+
+        public async Task<IActionResult> Create()
+        {
+
+
+
+        }
+
+    }
+}
