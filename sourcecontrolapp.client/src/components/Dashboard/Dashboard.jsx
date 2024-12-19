b6e1513fdb5af2995b6fde7a2b0354843b09ac04
Georgi Hadzhiev <georgi.hadzhiev@abv.bg>
2024-12-19 11:35:33 +0200

    Removed the "MyProfile" component from there...for now

--- sourcecontrolapp.client/src/components/Dashboard/Dashboard.jsx
+++ sourcecontrolapp.client/src/components/Dashboard/Dashboard.jsx
@@ -1,9 +1,9 @@
-import MyProfile from '../../components/MyProfile/MyProfile.jsx'
+
 
 function Dashboard() {
     return (
         <>
-            <MyProfile/>
+
             <p>Hello world!</p>
         </>
   );
