41b02e5310fecd1065c0a7fe4d0623bc88d7461a
Georgi Hadzhiev <georgi.hadzhiev@abv.bg>
2024-12-21 08:30:56 +0200

    left a comment explaining exactly how the "request" function and it's derivitives work

--- sourcecontrolapp.client/src/services/repoService.jsx
+++ sourcecontrolapp.client/src/services/repoService.jsx
@@ -1,8 +1,12 @@
 import * as request from  './requester.jsx'
 const baseUrl = 'https://localhost:7035/Repo'
 
-const create = async (albumData) => request.post(`${baseUrl}/Create`, albumData,false)
-
+// The "request" function and it's extentions take in the following params:
+// 1st param - The url endpoint you want to shoot to, as a string.
+// 2nd param - the given data you want to send, as an object.
+// 3rd param - a bool flag for whether or not the given user owns the data that's being requested. 
+// If any params are not relevant to the given request they can be left out. 
+const create = async (albumData) => request.post(`${baseUrl}/Create`, albumData)
 
 
 const repoService = {
