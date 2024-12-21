39ca7d5e8e113386c0b2ab861f32ff48133eae3a
Georgi Hadzhiev <georgi.hadzhiev@abv.bg>
2024-12-21 07:56:23 +0200

    removed the old "create" function in the "repoService" file as I've replaced it with the new requester

--- sourcecontrolapp.client/src/services/repoService.jsx
+++ sourcecontrolapp.client/src/services/repoService.jsx
@@ -3,31 +3,6 @@ const baseUrl = 'https://localhost:7035/Repo'
 
 const create = async (albumData) => request.post(`${baseUrl}/Create`, albumData,false)
 
-async function createOld(code, repoName, description, visibility, contributors, UserId) {
-
-    let res = await fetch(`${baseUrl}/Create`, {
-        method: 'POST',
-        headers: {
-            'content-type': 'application/json',
-            'X-Authorization': token,
-        },
-        body: JSON.stringify({ code, repoName, description, visibility, contributors, UserId })
-    })
-
-    let jsonResult = await res.json();
-
-    if (res.ok) {
-
-        return jsonResult;
-
-    }
-    else {
-
-        throw jsonResult.message;
-
-    }
-
-}
 
 
 const repoService = {
