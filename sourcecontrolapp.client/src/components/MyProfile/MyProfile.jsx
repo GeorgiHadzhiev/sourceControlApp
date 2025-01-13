f3061342388fd6ceba354e970a55484d5849e166
Georgi Hadzhiev <georgi.hadzhiev@abv.bg>
2025-01-13 04:05:23 +0200

    removed a "height" property from the itemsList class and added a margin-top for the sideContainer class

--- sourcecontrolapp.client/src/components/MyProfile/MyProfile.jsx
+++ sourcecontrolapp.client/src/components/MyProfile/MyProfile.jsx
@@ -60,6 +60,54 @@ function MyProfile() {
                                 <p>Language: JavaScript </p>
                             </article>
                         </li>
+                        <li className={`${classes.profileItem}`}>
+                            <article className={`${classes.profileItemArticle}`}>
+                                <div className={`${classes.profileItemArticleTop}`}>
+                                    <h3>Album Repo</h3>
+                                    <span id={`${classes.visSpanContainer}`}>
+                                        <span id={`${classes.visSpan}`}>Public</span>
+                                    </span>
+                                </div>
+                                <p>For my Final ReactJS exam</p>
+                                <p>Language: JavaScript </p>
+                            </article>
+                        </li>
+                        <li className={`${classes.profileItem}`}>
+                            <article className={`${classes.profileItemArticle}`}>
+                                <div className={`${classes.profileItemArticleTop}`}>
+                                    <h3>Album Repo</h3>
+                                    <span id={`${classes.visSpanContainer}`}>
+                                        <span id={`${classes.visSpan}`}>Public</span>
+                                    </span>
+                                </div>
+                                <p>For my Final ReactJS exam</p>
+                                <p>Language: JavaScript </p>
+                            </article>
+                        </li>
+                        <li className={`${classes.profileItem}`}>
+                            <article className={`${classes.profileItemArticle}`}>
+                                <div className={`${classes.profileItemArticleTop}`}>
+                                    <h3>Album Repo</h3>
+                                    <span id={`${classes.visSpanContainer}`}>
+                                        <span id={`${classes.visSpan}`}>Public</span>
+                                    </span>
+                                </div>
+                                <p>For my Final ReactJS exam</p>
+                                <p>Language: JavaScript </p>
+                            </article>
+                        </li>
+                        <li className={`${classes.profileItem}`}>
+                            <article className={`${classes.profileItemArticle}`}>
+                                <div className={`${classes.profileItemArticleTop}`}>
+                                    <h3>Album Repo</h3>
+                                    <span id={`${classes.visSpanContainer}`}>
+                                        <span id={`${classes.visSpan}`}>Public</span>
+                                    </span>
+                                </div>
+                                <p>For my Final ReactJS exam</p>
+                                <p>Language: JavaScript </p>
+                            </article>
+                        </li>
                     </ol>
                 </main>
 
