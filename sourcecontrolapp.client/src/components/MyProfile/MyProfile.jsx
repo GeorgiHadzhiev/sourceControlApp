8689cbae04f4a58996b0d126a6241c73e9f36e0b
Georgi Hadzhiev <georgi.hadzhiev@abv.bg>
2025-01-13 03:12:48 +0200

    added paddings and margins to the list items so they can be spaced

--- sourcecontrolapp.client/src/components/MyProfile/MyProfile.jsx
+++ sourcecontrolapp.client/src/components/MyProfile/MyProfile.jsx
@@ -46,7 +46,19 @@ function MyProfile() {
                                 </div>
                                 <p>For my Final ReactJS exam</p>
                                 <p>Language: JavaScript </p>
-                            </article>                     
+                            </article> 
+                        </li>
+                        <li className={`${classes.profileItem}`}>
+                            <article className={`${classes.profileItemArticle}`}>
+                                <div className={`${classes.profileItemArticleTop}`}>
+                                    <h3>Album Repo</h3>
+                                    <span id={`${classes.visSpanContainer}`}>
+                                        <span id={`${classes.visSpan}`}>Public</span>
+                                    </span>
+                                </div>
+                                <p>For my Final ReactJS exam</p>
+                                <p>Language: JavaScript </p>
+                            </article>
                         </li>
                     </ol>
                 </main>
