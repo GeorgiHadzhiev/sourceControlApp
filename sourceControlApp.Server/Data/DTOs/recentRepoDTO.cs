9a6292d1f82b811f61dff4ba37326a29d389e161
Georgi Hadzhiev <georgi.hadzhiev@abv.bg>
2025-02-10 01:30:08 +0200

    created a "recentRepoDTO" plus a folder for DTOs. Added needed props

---
+++ sourceControlApp.Server/Data/DTOs/recentRepoDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace sourceControlApp.Server.Data.DTOs
+{
+    public class recentRepoDTO
+    {
+
+        public string RepoName { get; set; } = string.Empty;
+        public bool Visibility { get; set; }
+        public string Description { get; set; } = string.Empty;
+
+    }
+}
