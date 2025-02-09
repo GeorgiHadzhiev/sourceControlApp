0030663b180ef8f9f93d725f26e2c87007c2a4e7
Georgi Hadzhiev <georgi.hadzhiev@abv.bg>
2025-02-09 22:40:51 +0200

    Created a "RepoCard" component for the recent repoes template

---
+++ sourcecontrolapp.client/src/components/MyProfile/RepoCard/RepoCard.jsx
@@ -0,0 +1,23 @@
+/* eslint-disable react-refresh/only-export-components */
+import classes from './MyProfile.module.css'
+function RepoCard() {
+
+    return (
+
+        <li className={`${classes.profileItem}`}>
+            <article className={`${classes.profileItemArticle}`}>
+                <div className={`${classes.profileItemArticleTop}`}>
+                    <h3>Album Repo</h3>
+                    <span id={`${classes.visSpanContainer}`}>
+                        <span id={`${classes.visSpan}`}>Public</span>
+                    </span>
+                </div>
+                <p>For my Final ReactJS exam</p>
+                <p>Language: JavaScript </p>
+            </article>
+        </li>
+        
+    )
+
+}
+export default RepoCard()
